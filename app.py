4f80cf7d81f278faef2d450534bbb9e2fb864d0f
bajrulhaqcbe-coder <bajrulhaq.cbe@gmail.com>
2026-04-14 22:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("🎓 Polling Officers Search System")
 # ------------------ LOAD DATA ------------------ #
 @st.cache_data
 def load_data():
-    df = pd.read_excel("data.xlsx")
+    df = pd.read_excel("2ND TRAINING ROOMS.xlsx")
     df.columns = df.columns.str.strip()
 
     for col in df.columns:
